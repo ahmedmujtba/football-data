c6ee7173533141e5e9724a19c580b38b20c0b35d
Ahmed Mujtba <ahmedmujtba@outlook.com>
2023-02-03 21:54:18 +0000

    key removed

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const options = {
   method: "GET",
   url: "https://api-football-v1.p.rapidapi.com/v3/",
-  headers: {
-    "X-RapidAPI-Key": `330691264dmshf5c00984330162bp131752jsnae4eb909e5c2`,
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-  },
 };
 
 export function getCountries() {
